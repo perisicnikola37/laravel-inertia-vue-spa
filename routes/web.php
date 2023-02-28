3026f51f46eda4c4d4f8fea6690211e968d47266
perisicnikola37 <perisicnikola37@gmail.com>
2023-02-28 23:49:40 +0100

    Create users Index.vue component

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -23,6 +14,9 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+
+Route::resource('/users', UserController::class);
+Route::resource('/roles', RoleController::class);
 
 Route::middleware([
     'auth:sanctum',
