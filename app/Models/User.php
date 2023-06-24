bfc570b147dcbb4306982f69ea0bde08c1c069b3
Nikola <perisicnikola37@gmail.com>
2023-06-25 01:28:43 +0200

    Update models by removing unnecessary code

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
     use Notifiable;
@@ -19,22 +17,6 @@ class User extends Authenticatable
 
     protected $guarded = [];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    // protected $fillable = [
-    //     'name',
-    //     'email',
-    //     'password',
-    // ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
@@ -42,26 +24,12 @@ class User extends Authenticatable
         'two_factor_secret',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array<int, string>
-     */
     protected $appends = [
         'profile_photo_url',
         'formatted_created_at'
     ];
 
-    public function getFormattedCreatedAtAttribute()
+    public function getFormattedCreatedAtAttribute(): string
     {
         return $this->created_at->format('d M Y');
     }
@@ -71,7 +39,7 @@ class User extends Authenticatable
         return $this->belongsTo(Role::class);
     }
 
-    public function isAdmin()
+    public function isAdmin(): bool
     {
         return $this->role->id == 1;
     }
