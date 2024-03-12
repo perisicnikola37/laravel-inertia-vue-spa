4ef6e2bc5cee5755e09d0bc2ba51c977456517ff
Nikola <perisicnikola37@gmail.com>
2024-03-12 13:47:04 +0100

    Create UserManagementTest.php file

--- _ide_helper.php
+++ _ide_helper.php
@@ -4428,6 +4428,93 @@ namespace Illuminate\Support\Facades {
             /**
      * 
      *
+     * @see \Illuminate\Encryption\Encrypter
+     */        class Crypt {
+                    /**
+         * Determine if the given key and cipher combination is valid.
+         *
+         * @param string $key
+         * @param string $cipher
+         * @return bool 
+         * @static 
+         */        public static function supported($key, $cipher)
+        {
+                        return \Illuminate\Encryption\Encrypter::supported($key, $cipher);
+        }
+                    /**
+         * Create a new encryption key for the given cipher.
+         *
+         * @param string $cipher
+         * @return string 
+         * @static 
+         */        public static function generateKey($cipher)
+        {
+                        return \Illuminate\Encryption\Encrypter::generateKey($cipher);
+        }
+                    /**
+         * Encrypt the given value.
+         *
+         * @param mixed $value
+         * @param bool $serialize
+         * @return string 
+         * @throws \Illuminate\Contracts\Encryption\EncryptException
+         * @static 
+         */        public static function encrypt($value, $serialize = true)
+        {
+                        /** @var \Illuminate\Encryption\Encrypter $instance */
+                        return $instance->encrypt($value, $serialize);
+        }
+                    /**
+         * Encrypt a string without serialization.
+         *
+         * @param string $value
+         * @return string 
+         * @throws \Illuminate\Contracts\Encryption\EncryptException
+         * @static 
+         */        public static function encryptString($value)
+        {
+                        /** @var \Illuminate\Encryption\Encrypter $instance */
+                        return $instance->encryptString($value);
+        }
+                    /**
+         * Decrypt the given value.
+         *
+         * @param string $payload
+         * @param bool $unserialize
+         * @return mixed 
+         * @throws \Illuminate\Contracts\Encryption\DecryptException
+         * @static 
+         */        public static function decrypt($payload, $unserialize = true)
+        {
+                        /** @var \Illuminate\Encryption\Encrypter $instance */
+                        return $instance->decrypt($payload, $unserialize);
+        }
+                    /**
+         * Decrypt the given string without unserialization.
+         *
+         * @param string $payload
+         * @return string 
+         * @throws \Illuminate\Contracts\Encryption\DecryptException
+         * @static 
+         */        public static function decryptString($payload)
+        {
+                        /** @var \Illuminate\Encryption\Encrypter $instance */
+                        return $instance->decryptString($payload);
+        }
+                    /**
+         * Get the encryption key that the encrypter is currently using.
+         *
+         * @return string 
+         * @static 
+         */        public static function getKey()
+        {
+                        /** @var \Illuminate\Encryption\Encrypter $instance */
+                        return $instance->getKey();
+        }
+            }
+            /**
+     * 
+     *
      * @see https://carbon.nesbot.com/docs/
      * @see https://github.com/briannesbitt/Carbon/blob/master/src/Carbon/Factory.php
      * @method static \Illuminate\Support\Carbon create($year = 0, $month = 1, $day = 1, $hour = 0, $minute = 0, $second = 0, $tz = null)
@@ -17638,6 +17725,7 @@ namespace  {
             class Cache extends \Illuminate\Support\Facades\Cache {}
             class Config extends \Illuminate\Support\Facades\Config {}
             class Cookie extends \Illuminate\Support\Facades\Cookie {}
+            class Crypt extends \Illuminate\Support\Facades\Crypt {}
             class Date extends \Illuminate\Support\Facades\Date {}
             class DB extends \Illuminate\Support\Facades\DB {}
             class Eloquent extends \Illuminate\Database\Eloquent\Model {                            /**
---
+++ tests/Feature/UserManagementTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserManagementTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_validation()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->post('/users', []);
+
+        $response->assertSessionHasErrors(['name', 'email', 'password', 'avatar']);
+    }
+
+    public function test_user_update_feature()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $user = User::factory()->create();
+
+        $this->put('/users/' . $user->id, [
+            'name' => 'John Doe',
+            'email' => 'updated@mail.com']);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $user->id,
+            'name' => 'John Doe',
+            'email' => 'updated@mail.com']
+        );
+    }
+
+    public function test_user_delete_feature()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $user = User::factory()->create();
+
+        $response = $this->delete('/users/' . $user->id);
+
+        $response->assertRedirect('/');
+        $this->assertDatabaseMissing('users', ['id' => $user->id]);
+    }
+
+    public function test_user_delete_feature_with_invalid_id()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->withHeaders(['X-CSRF-TOKEN' => ''])
+            ->delete('/users/999');
+
+        $response->assertStatus(302);
+    }
+
+    public function test_all_users_are_shown_on_index_page()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $user = User::factory()->create();
+
+        $response = $this->get('/users');
+
+        $response->assertSee($user->name);
+    }
+
+    public function test_user_creation_date_is_in_valid_format()
+    {
+        $this->actingAs(User::factory()->create());
+
+        $user = User::factory()->create();
+
+        $response = $this->get('/users');
+
+        $response->assertSee($user->created_at->format('d M Y'));
+    }
+}
